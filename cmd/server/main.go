4816880e0738c86fc60e268a17033f0cc7f70c5c
sojoudian <sojodyan@gmail.com>
2022-03-21 22:43:48 -0400

    update with docker compose and Taskfile

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/sojoudian/goRestAPIv2/internal/db"
+)
 
 // Run - is goin to be responsible for
 // the instantiation and startup of the
 // go application
-fun Run() error {
+func Run() error {
 	fmt.Println("starting up our application")
+	fmt.Println("##################################################")
+	db, err := db.NewDatabase()
+	if err != nil {
+		fmt.Println("Faild to connect to the database")
+		return err
+	}
+	if err := db.Ping(context.Background()); err != nil {
+		return err
+	}
+	fmt.Println("successfully connected and pinged the database!")
 	return nil
 }
 
